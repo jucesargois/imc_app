3d1fa6d19711344b8244e4aa4d07349331b9607c
Julio <jucesargois@gmail.com>
2022-02-02 23:24:05 -0300

    Criado componente home e inserido na main do projeto

---
+++ lib/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child:  Text('App Imc ')),
+      ),
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
