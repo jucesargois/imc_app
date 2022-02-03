68a785bd696ad8796e6592512424ce7a88d0d447
Julio <jucesargois@gmail.com>
2022-02-03 00:53:43 -0300

    Inserido os elementos visuais e funcionais da tela home

--- lib/home.dart
+++ lib/home.dart
@@ -1,16 +1,85 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+  Home({Key? key}) : super(key: key);
 
+  int? peso;
+  double? altura;
+  var resultado;
+  calculaImc(int? peso, double? altura) {
+    var resultado;
+    if (peso != null && altura != null) {
+      return resultado = peso / (altura * altura);
+    }
+  }
+
+  TextEditingController pesocontroller = TextEditingController();
+  TextEditingController alturacontroller = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Center(child:  Text('App Imc ')),
-      ),
-      body: Column(
-        children: [],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Center(child: Text('App Imc ')),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 130,
+              ),
+              const Center(
+                child: Text(
+                  'Peso',
+                  style: TextStyle(fontSize: 28),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: pesocontroller,
+                  decoration: const InputDecoration(hintText: 'Peso'),
+                ),
+              ),
+              const SizedBox(
+                height: 100,
+              ),
+              const Center(
+                child: Text(
+                  'Altura',
+                  style: TextStyle(fontSize: 28),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: alturacontroller,
+                  decoration: const InputDecoration(hintText: 'altura'),
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Container(
+                width: 150,
+                child: ElevatedButton(
+                  onPressed: () {
+                    int? pesoconv = int.tryParse(pesocontroller.text);
+                    peso = pesoconv;
+                    double? alturaconv = double.tryParse(alturacontroller.text);
+                    altura = alturaconv;
+                    resultado = calculaImc(peso, altura);
+                  },
+                  child: const Text('Calcular'),
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Text("Resultado: $resultado")
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:imc_app/home.dart';
 
-void main() {
-  runApp(const Home());
+ main() {
+  runApp( Home());
 }
 
