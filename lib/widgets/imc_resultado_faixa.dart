48d461b624bbe723f683ba2d35689240eb47d2c6
Julio <jucesargois@gmail.com>
2022-02-07 00:09:47 -0300

    Criado widget  ImcResultadoWidget para a faixa do resultado do imc

---
+++ lib/widgets/imc_resultado_faixa.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:imc_app/home.dart';
+
+import '../classificacao_imc.dart';
+
+class ImcResultadoWidget extends StatelessWidget {
+  const ImcResultadoWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Visibility(
+      visible: value,
+      child: Container(
+        width: double.infinity,
+        height: 50,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          color: classificacaoImc(resultado) == 'Adequado'
+              ? Colors.green
+              : Colors.red,
+        ),
+        child: Center(
+          child: Text(
+            '${classificacaoImc(resultado)}',
+            style: const TextStyle(color: Colors.white, fontSize: 25),
+          ),
+        ),
+      ),
+    );
+  }
+}
