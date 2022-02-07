fac837aa5c20883bc033dbe8b8bea23d6603ab21
Julio <jucesargois@gmail.com>
2022-02-07 00:06:39 -0300

    Função calculaImc movida para um arquivo separado

---
+++ lib/calcula_imc.dart
@@ -0,0 +1,8 @@
+import 'package:imc_app/home.dart';
+
+calculaImc(int? peso, double? altura) {
+    if (peso != null && altura != null) {
+      return resultado = peso / (altura * altura);
+    }
+  }
+  
